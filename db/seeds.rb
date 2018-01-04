4612f8d4006cf6b37804dfc1e038e4a5f67a748d
Shota HAMADA <maku693@outlook.com>
2018-01-05 03:00:21 +0900

    Create demo event as seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(title: 'Demo Event', body: 'This is a demo event.', password: 'password')
