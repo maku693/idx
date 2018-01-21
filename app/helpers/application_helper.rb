c14f2984914a2dc04ab374a72e72f5f99f53c2ed
Shota HAMADA <maku3ts@gmail.com>
2018-01-21 15:56:04 +0900

    Apply styles to new event page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title_tag(application_name)
     tag.title([content_for(:title), application_name].compact.join(' - '))
   end
+
+  def page_title(title = nil)
+    title.nil? ? content_for(:title) : content_for(:title, title)
+  end
 end
