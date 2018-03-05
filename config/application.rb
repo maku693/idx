b32b8f82c3fc4027eb03472f37783a191a0986cb
Shota HAMADA <maku693@outlook.com>
2018-03-06 01:33:31 +0900

    Set application time zone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Set application time zone before related classes are initialized.
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
